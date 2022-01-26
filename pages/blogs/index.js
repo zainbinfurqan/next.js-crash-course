86a6ba5172de09b734f8d667176edae3eb272cf2
Zain Ahmed <zain.ahmed@backcountry.com>
2022-01-26 18:39:10 +0500

    update image optimize code

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,13 +1,31 @@
+import Image from "next/image";
 import React from "react";
 import AuthorizedLayout from "../../layout/authorized-layout";
 import CommonLayout from "../../layout/common";
+import Scenry from "../../assets/images/211836.jpeg";
 
 function Blogs(props) {
+  const images = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   return (
     <CommonLayout>
       <AuthorizedLayout>
         <div>
           <h2>Blogs</h2>
+          {images.map((itm, ind) => {
+            return <img src='/211836.jpeg'/>;
+            // return <Image src={Scenry} />;
+            // return (
+            //   <Image
+            //     src="https://i.ibb.co/RjLTJvN/211836.jpg"
+            //     alt="https://i.ibb.co/RjLTJvN/211836.jpg"
+            //     width={600}
+            //     height={450}
+            //     loading="eager"
+            //     layout="fixed"
+            //   />
+            // );
+          })}
         </div>
       </AuthorizedLayout>
     </CommonLayout>
